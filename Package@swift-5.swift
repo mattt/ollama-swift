51284b4da2612697311fa0986e0259d2680b2763
Mattt Zmuda <mattt@me.com>
2025-02-15 05:45:01 -0800

    Revert platform list in Swift 5 package manifest

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,18 +1,12 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
     name: "Ollama",
     platforms: [
-        .macOS(.v13),
-        .macCatalyst(.v13),
-        .iOS(.v16),
-        .watchOS(.v9),
-        .tvOS(.v16),
-        .visionOS(.v1),
+        .macOS(.v13)
     ],
     products: [
         .library(
@@ -21,7 +15,6 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Ollama",
-            dependencies: [])
+            name: "Ollama")
     ]
 )

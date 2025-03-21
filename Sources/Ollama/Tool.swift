450336803452d993ffa7c3b234d83785a0e7b03a
Mattt <mattt@loopwork.com>
2025-03-21 14:17:15 -0700

    Fix example JSON Schema in documentation comment for Tool.schema property (#13)

--- Sources/Ollama/Tool.swift
+++ Sources/Ollama/Tool.swift
@@ -22,22 +22,25 @@ public struct Tool<Input: Codable, Output: Codable>: ToolProtocol {
     /// ```swift
     /// var schema: [String: Value] {
     ///     [
-    ///         "name": "get_current_weather",
-    ///         "description": "Get the current weather for a location",
-    ///         "parameters": [
-    ///             "type": "object",
-    ///             "properties": [
-    ///                 "location": [
-    ///                     "type": "string",
-    ///                     "description": "The location to get the weather for, e.g. San Francisco, CA"
+    ///         "type: "function",
+    ///         "function": [
+    ///             "name": "get_current_weather",
+    ///             "description": "Get the current weather for a location",
+    ///             "parameters": [
+    ///                 "type": "object",
+    ///                 "properties": [
+    ///                     "location": [
+    ///                         "type": "string",
+    ///                         "description": "The location to get the weather for, e.g. San Francisco, CA"
+    ///                     ],
+    ///                     "format": [
+    ///                         "type": "string",
+    ///                         "description": "The format to return the weather in, e.g. 'celsius' or 'fahrenheit'",
+    ///                         "enum": ["celsius", "fahrenheit"]
+    ///                     ]
     ///                 ],
-    ///                 "format": [
-    ///                     "type": "string",
-    ///                     "description": "The format to return the weather in, e.g. 'celsius' or 'fahrenheit'",
-    ///                     "enum": ["celsius", "fahrenheit"]
-    ///                 ]
-    ///             ],
-    ///             "required": ["location", "format"]
+    ///                 "required": ["location", "format"]
+    ///             ]
     ///         ]
     ///     ]
     /// }
